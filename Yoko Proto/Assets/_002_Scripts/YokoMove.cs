f3b2d265df5a36ef18cbeeba28e4099ab9176ae6
Valerian Lambert <valerian.lambert@brassart.fr>
2022-05-19 15:08:39 +0200

    Raycast Plright

--- Yoko Proto/Assets/_002_Scripts/YokoMove.cs
+++ Yoko Proto/Assets/_002_Scripts/YokoMove.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class YokoMove : MonoBehaviour
 {
-    int speed;
+    public float moveTime;
+    public GameObject[] Yoko;
+    public int moveBy = 1;
+    public bool isGameStarted;
     void Start()
     {
-        
+        Yoko = GameObject.FindGameObjectsWithTag("Yoko");
+        moveTime = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveTime <= 0 && isGameStarted)
+        {
+            Yoko[0].transform.Translate(moveBy, 0, 0);
+            Yoko[1].transform.Translate(moveBy, 0, 0);
+            Yoko[2].transform.Translate(moveBy, 0, 0);
+            moveTime = 1;
+        }
+        else moveTime -= Time.deltaTime;
     }
 }
