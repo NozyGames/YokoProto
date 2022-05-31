8ee15c9ea72bf5073f2ff5c92447c3bf4b2782e5
Valerian Lambert <valerian.lambert@brassart.fr>
2022-05-31 15:34:39 +0200

    refonte déplacement Yoko et Raycast

---
+++ Yoko Proto/Assets/_002_Scripts/Raycasts/EntityRaycast.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityRaycast : MonoBehaviour
+{
+    public LayerMask hitLayer;
+    [SerializeField]
+    private Directions direction;
+    public Directions EntityDirection { get { return direction; } }
+    private Vector2 entiDirection;
+    private void OnDrawGizmos()
+    {
+        //Je debug un rayon rouge qui représente le Raycast (uniquement sur l'éditeur)
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + (Vector3)(entiDirection * 1), transform.position + (Vector3)(entiDirection * 1.2f));
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position + (Vector3.right * 1), transform.position + (Vector3.right * 1.2f));
+    }
+    void Start()
+    {
+        switch (direction)
+        {
+            case Directions.Up:
+                entiDirection = Vector2.up;
+                break;
+            case Directions.Down:
+                entiDirection = Vector2.down;
+                break;
+            case Directions.Right:
+                entiDirection = Vector2.right;
+                break;
+            case Directions.Left:
+                entiDirection = Vector2.left;
+                break;
+        }
+    }
+    public void CheckRaycast()
+    {
+        //Je créé un Raycast nommé Hit qui part de la position du Yoko et va vers le bas sur une distance de 1 et qui ne touche que les Kyokos
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + (Vector3)(entiDirection * 1), transform.position + (Vector3)(entiDirection * 1.2f), 1, hitLayer);
+
+        if (hit)
+        {
+            if (hit.transform.GetComponent<EntityRaycast>())
+            {
+                Directions tempDir = hit.transform.GetComponent<EntityRaycast>().EntityDirection;
+                //Quand le Raycast rentre en contact avec une cible, je log le nom de la cible et la détruit
+                Debug.Log("Hit " + hit.collider.name);
+                Destroy(hit.transform.gameObject);
+                switch (EntityDirection)
+                {
+                    case Directions.Up:
+                        if (tempDir == Directions.Down) Destroy(gameObject);
+                        break;
+                    case Directions.Down:
+                        if (tempDir == Directions.Up) Destroy(gameObject);
+                        break;
+                    case Directions.Right:
+                        if (tempDir == Directions.Left) Destroy(gameObject);
+                        break;
+                    case Directions.Left:
+                        if (tempDir == Directions.Right) Destroy(gameObject);
+                        break;
+                }
+            }
+        }
+    }
+    public void OnDestroy()
+    {
+        YokoMove.Instance.RemoveEntity(this);
+        YokoMove.Instance.RemoveYoko(gameObject);
+    }
+    public bool IsBlocked()
+    {
+        RaycastHit2D blocker = Physics2D.Raycast(transform.position + (Vector3.right * 1), transform.position + (Vector3.right * 1.2f), 1);
+        Debug.Log(blocker.transform.name + " blocked: " + name);
+        if (blocker.transform == transform) return false;
+        return blocker;
+    }
+}
--- Yoko Proto/Assets/_002_Scripts/YokoMove.cs
+++ Yoko Proto/Assets/_002_Scripts/YokoMove.cs
@@ -1,28 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class YokoMove : MonoBehaviour
 {
+    public static YokoMove Instance { get; private set; }
     public float moveTime;
-    public GameObject Yoko;
     public int moveBy = 1;
     public bool isGameStarted;
+    private List<EntityRaycast> allEnti = new List<EntityRaycast>();
+    private List<GameObject> allYokos = new List<GameObject>();
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
-        //Je récupère mon objet Yoko (qui sera en l'occurrence le "Loader") via le tag "Yoko"
-        Yoko = GameObject.FindGameObjectWithTag("Yoko");
-        moveTime = 1;
+        //Je récupère tous mes Yoko et je les mets dans une list
+        allYokos.AddRange(GameObject.FindGameObjectsWithTag("Yoko"));
+        moveTime = 1.75f;
+        allEnti.AddRange(FindObjectsOfType<EntityRaycast>());
     }
 
     void Update()
     {
-        //Si "moveTime" est à 0 ou inférieur et que le bouton a été appuyé, je déplace mon objet Yoko de "moveBy" (int modifiable) en x
-        if (moveTime <= 0 && isGameStarted)
+        //Quand le bouton a été appuyé, j'applique un décompte en temps réel à moveTime
+        if (isGameStarted)
         {
-            Yoko.transform.Translate(moveBy, 0, 0);
-            moveTime = 1;
+            moveTime -= Time.deltaTime;
+            //Si "moveTime" est égal ou inférieur à 0, je déplace mon objet Yoko de "moveBy" (int modifiable) en x
+            if (moveTime <= 0)
+            {
+                foreach (GameObject yokos in allYokos)
+                {
+                    if (!yokos.GetComponent<EntityRaycast>().IsBlocked())
+                    yokos.transform.Translate(moveBy, 0, 0);
+                }
+                moveTime = 1;
+                foreach (EntityRaycast entity in allEnti)
+                {
+                    entity.CheckRaycast();
+                }
+            }
         }
-        else moveTime -= Time.deltaTime;
+    }
+    public void RemoveEntity(EntityRaycast entity)
+    {
+        allEnti.Remove(entity);
+    }
+
+    public void RemoveYoko(GameObject yoko)
+    {
+        allYokos.Remove(yoko);
     }
 }
